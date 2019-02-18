30086bbf1905a8a3932fba0f0a47e299c475a64d
lizijie <562949697@qq.com>
2019-02-19 01:03:22 +0800

    项目

---
+++ React项目/react-mall/src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+         12312321
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ React项目/react-mall/src/components/Header/Header.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+export class Header extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default Header;
+
+ 
+
