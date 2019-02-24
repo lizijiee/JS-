ec99a5769fbd24da17131cf18f4911f13282d0ba
lizijie <562949697@qq.com>
2019-02-24 09:29:40 +0800

    项目更新

--- React项目/react-mall/src/components/Content/Food/List/List.js
+++ React项目/react-mall/src/components/Content/Food/List/List.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react'
- import { Button, Pagination,Switch,Checkbox} from 'antd';
+import { Button, Pagination, Switch, Checkbox, Select } from 'antd';
 import IconFont from '../../../../iconfont/font'
 import './List.less';
 import jiding from './img/mall-food-list-gongbao.jpg'
+
+const Option = Select.Option;
+
 export default class Temp extends Component {
     render() {
         return (
@@ -20,6 +23,33 @@ export default class Temp extends Component {
                                 <span>输入检索:</span>
                                 <input type="text" placeholder="请输入姓名">
                                 </input>
+                                
+                            </div>
+                            <div className="el-form-item">
+                            <span>菜品类型:</span>
+                            <Select
+                                    showSearch
+                                    placeholder="菜品类型:"
+                                    style={{ width: 170,color:"#606266",fontSize:12 }}
+                                >
+                                    <Option value="特色汤面">特色汤面</Option>
+                                    <Option value="酒水饮料">酒水饮料</Option>
+                                    <Option value="烧烤系列">烧烤系列</Option>
+                                    <Option value="美味盖饭">美味盖饭</Option>
+                                    <Option value="小吃，肉夹馍">小吃，肉夹馍</Option>
+                                </Select>,
+                            </div>
+                            <div className="el-form-item">
+                            <span>菜品类型:</span>
+                            <Select
+                                    showSearch
+                                    placeholder="上架状态"
+                                    style={{ width: 170,color:"#606266",fontSize:12,marginRight:"0!imoprtant"}}>
+                                    <Option value="热销">热销</Option>
+                                    <Option value="买过">买过</Option>
+                                    <Option value="优惠">优惠</Option>
+                                    <Option value="为您优选">为您优选</Option>
+                                </Select>,
                             </div>
                         </div>
                     </div>
@@ -34,7 +64,7 @@ export default class Temp extends Component {
                         <table cellPadding="0" cellSpacing="0">
                             <thead>
                                 <tr>
-                                    <th> < Checkbox/></th>
+                                    <th> < Checkbox /></th>
                                     <th>编号</th>
                                     <th>菜品图片</th>
                                     <th>菜品名称</th>
@@ -49,13 +79,13 @@ export default class Temp extends Component {
                                     <td>< Checkbox /></td>
                                     <td>1</td>
                                     <td><img src={jiding} /></td>
-                                    <td>宫保鸡丁</td> 
-                                    <td style={{padding:"0 5px",textAlign:"left",textIndent:'1em'}}> 宫保鸡丁（Kung Pao Chicken），是一道闻名中外的特色传统名菜。鲁菜、川菜、贵州菜中都有收录，原料、做法有差别。</td>
+                                    <td>宫保鸡丁</td>
+                                    <td style={{ padding: "0 5px", textAlign: "left", textIndent: '1em' }}> 宫保鸡丁（Kung Pao Chicken），是一道闻名中外的特色传统名菜。鲁菜、川菜、贵州菜中都有收录，原料、做法有差别。</td>
                                     <td>￥50</td>
                                     <td>
-                                        <p><span style={{paddingRight:15}}>热销:</span> <Switch size="default" /></p>
-                                        <p><span style={{paddingRight:15}}>新品:</span><Switch size="default"/></p>
-                                        <p><span style={{paddingRight:15}}>优惠:</span><Switch size="default"/></p>
+                                        <p><span style={{ paddingRight: 15 }}>热销:</span> <Switch size="default" /></p>
+                                        <p><span style={{ paddingRight: 15 }}>新品:</span><Switch size="default" /></p>
+                                        <p><span style={{ paddingRight: 15 }}>优惠:</span><Switch size="default" /></p>
                                     </td>
                                     <td>
                                         <Button type="primary" size="small" style={{ marginRight: 10, fontSize: 13, width: 60, height: 25, borderRadius: 5 }}>编辑</Button>
@@ -66,13 +96,13 @@ export default class Temp extends Component {
                                     <td>< Checkbox /></td>
                                     <td>1</td>
                                     <td><img src={jiding} /></td>
-                                    <td>宫保鸡丁</td> 
-                                    <td style={{padding:"0 5px",textAlign:"left",textIndent:'1em'}}> 宫保鸡丁（Kung Pao Chicken），是一道闻名中外的特色传统名菜。鲁菜、川菜、贵州菜中都有收录，原料、做法有差别。</td>
+                                    <td>宫保鸡丁</td>
+                                    <td style={{ padding: "0 5px", textAlign: "left", textIndent: '1em' }}> 宫保鸡丁（Kung Pao Chicken），是一道闻名中外的特色传统名菜。鲁菜、川菜、贵州菜中都有收录，原料、做法有差别。</td>
                                     <td>￥50</td>
                                     <td>
-                                        <p><span style={{paddingRight:15}}>热销:</span> <Switch size="default" /></p>
-                                        <p><span style={{paddingRight:15}}>新品:</span><Switch size="default"/></p>
-                                        <p><span style={{paddingRight:15}}>优惠:</span><Switch size="default"/></p>
+                                        <p><span style={{ paddingRight: 15 }}>热销:</span> <Switch size="default" /></p>
+                                        <p><span style={{ paddingRight: 15 }}>新品:</span><Switch size="default" /></p>
+                                        <p><span style={{ paddingRight: 15 }}>优惠:</span><Switch size="default" /></p>
                                     </td>
                                     <td>
                                         <Button type="primary" size="small" style={{ marginRight: 10, fontSize: 13, width: 60, height: 25, borderRadius: 5 }}>编辑</Button>
@@ -83,13 +113,13 @@ export default class Temp extends Component {
                                     <td>< Checkbox /></td>
                                     <td>1</td>
                                     <td><img src={jiding} /></td>
-                                    <td>宫保鸡丁</td> 
-                                    <td style={{padding:"0 5px",textAlign:"left",textIndent:'1em'}}> 宫保鸡丁（Kung Pao Chicken），是一道闻名中外的特色传统名菜。鲁菜、川菜、贵州菜中都有收录，原料、做法有差别。</td>
+                                    <td>宫保鸡丁</td>
+                                    <td style={{ padding: "0 5px", textAlign: "left", textIndent: '1em' }}> 宫保鸡丁（Kung Pao Chicken），是一道闻名中外的特色传统名菜。鲁菜、川菜、贵州菜中都有收录，原料、做法有差别。</td>
                                     <td>￥50</td>
                                     <td>
-                                        <p><span style={{paddingRight:15}}>热销:</span> <Switch size="default" /></p>
-                                        <p><span style={{paddingRight:15}}>新品:</span><Switch size="default"/></p>
-                                        <p><span style={{paddingRight:15}}>优惠:</span><Switch size="default"/></p>
+                                        <p><span style={{ paddingRight: 15 }}>热销:</span> <Switch size="default" /></p>
+                                        <p><span style={{ paddingRight: 15 }}>新品:</span><Switch size="default" /></p>
+                                        <p><span style={{ paddingRight: 15 }}>优惠:</span><Switch size="default" /></p>
                                     </td>
                                     <td>
                                         <Button type="primary" size="small" style={{ marginRight: 10, fontSize: 13, width: 60, height: 25, borderRadius: 5 }}>编辑</Button>
@@ -100,13 +130,13 @@ export default class Temp extends Component {
                                     <td>< Checkbox /></td>
                                     <td>1</td>
                                     <td><img src={jiding} /></td>
-                                    <td>宫保鸡丁</td> 
-                                    <td style={{padding:"0 5px",textAlign:"left",textIndent:'1em'}}> 宫保鸡丁（Kung Pao Chicken），是一道闻名中外的特色传统名菜。鲁菜、川菜、贵州菜中都有收录，原料、做法有差别。</td>
+                                    <td>宫保鸡丁</td>
+                                    <td style={{ padding: "0 5px", textAlign: "left", textIndent: '1em' }}> 宫保鸡丁（Kung Pao Chicken），是一道闻名中外的特色传统名菜。鲁菜、川菜、贵州菜中都有收录，原料、做法有差别。</td>
                                     <td>￥50</td>
                                     <td>
-                                        <p><span style={{paddingRight:15}}>热销:</span> <Switch size="default" /></p>
-                                        <p><span style={{paddingRight:15}}>新品:</span><Switch size="default"/></p>
-                                        <p><span style={{paddingRight:15}}>优惠:</span><Switch size="default"/></p>
+                                        <p><span style={{ paddingRight: 15 }}>热销:</span> <Switch size="default" /></p>
+                                        <p><span style={{ paddingRight: 15 }}>新品:</span><Switch size="default" /></p>
+                                        <p><span style={{ paddingRight: 15 }}>优惠:</span><Switch size="default" /></p>
                                     </td>
                                     <td>
                                         <Button type="primary" size="small" style={{ marginRight: 10, fontSize: 13, width: 60, height: 25, borderRadius: 5 }}>编辑</Button>
@@ -117,13 +147,13 @@ export default class Temp extends Component {
                                     <td>< Checkbox /></td>
                                     <td>1</td>
                                     <td><img src={jiding} /></td>
-                                    <td>宫保鸡丁</td> 
-                                    <td style={{padding:"0 5px",textAlign:"left",textIndent:'1em'}}> 宫保鸡丁（Kung Pao Chicken），是一道闻名中外的特色传统名菜。鲁菜、川菜、贵州菜中都有收录，原料、做法有差别。</td>
+                                    <td>宫保鸡丁</td>
+                                    <td style={{ padding: "0 5px", textAlign: "left", textIndent: '1em' }}> 宫保鸡丁（Kung Pao Chicken），是一道闻名中外的特色传统名菜。鲁菜、川菜、贵州菜中都有收录，原料、做法有差别。</td>
                                     <td>￥50</td>
                                     <td>
-                                        <p><span style={{paddingRight:15}}>热销:</span> <Switch size="default" /></p>
-                                        <p><span style={{paddingRight:15}}>新品:</span><Switch size="default" /></p>
-                                        <p><span style={{paddingRight:15}}>优惠:</span><Switch size="default"/></p>
+                                        <p><span style={{ paddingRight: 15 }}>热销:</span> <Switch size="default" /></p>
+                                        <p><span style={{ paddingRight: 15 }}>新品:</span><Switch size="default" /></p>
+                                        <p><span style={{ paddingRight: 15 }}>优惠:</span><Switch size="default" /></p>
                                     </td>
                                     <td>
                                         <Button type="primary" size="small" style={{ marginRight: 10, fontSize: 13, width: 60, height: 25, borderRadius: 5 }}>编辑</Button>
