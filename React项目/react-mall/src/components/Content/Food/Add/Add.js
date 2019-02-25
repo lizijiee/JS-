bbd0106d7873a0a0f25148a461e6ae6ab7f4e28d
lizijie <562949697@qq.com>
2019-02-26 01:10:25 +0800

    项目更新

--- React项目/react-mall/src/components/Content/Food/Add/Add.js
+++ React项目/react-mall/src/components/Content/Food/Add/Add.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react'
 import "./Step/Add.less";
-import { Steps, Button, message } from 'antd';
+import { Steps, Button, message, Form } from 'antd';
 import StepOne from './Step/Step';
-  
+
 const Step = Steps.Step;
 const steps = [{
     title: '填写菜品信息',
@@ -15,11 +15,12 @@ const steps = [{
     content: 'Last-content',
 }];
 
-export default class Temp extends Component {
+class Temp extends Component {
     constructor() {
         super()
         this.state = {
             current: 0,
+            data:{}
         };
     }
     next() {
@@ -29,12 +30,31 @@ export default class Temp extends Component {
 
     prev() {
         const current = this.state.current - 1;
-        this.setState({ current });
+        this.setState({ current })
+        // console.log(this.state)
     }
+
+    handleSubmit(e) {
+        //表单内容提交组件,校验并获取一组输入域的值与 Error	
+        // e.preventDefault();
+        // console.log(this.props.form.getFieldError("name"))
+        this.props.form.validateFields((errors, values) => {
+            if (!!errors) {
+                message.error('验证失败!')
+                // console.log('Errors in form!!!');
+                return;
+            }
+            this.next()
+          let data = Object.assign({}, this.state.data, this.props.form.getFieldsValue())
+            // this.setState(data)
+            console.log(this.props.form.getFieldsValue())
+        });
+    };
+
     selest(params) {
         switch (params) {
             case 0:
-                return <StepOne/> ;
+                return <StepOne props={this.props} handleSubmit={this.handleSubmit} />;
                 break;
             case 1:
                 // return <StepTwo/> ;
@@ -46,23 +66,31 @@ export default class Temp extends Component {
     }
 
     render() {
-
         const { current } = this.state;
         return (
             <main className="food-add">
                 <div className="food-add-wrap">
-                    <Steps current={current} >
+                    <Steps current={current}   >
                         {steps.map(item => <Step key={item.title} title={item.title} />
                         )}
                     </Steps>
                     <div className="form-wrap">
-                    {/* 分步组件 */}
-                          {this.selest(current)}     
+                        {/* 分步组件 */}
+                        {this.selest(current)}
                     </div>
-                    <div className="steps-action">
+                    <div className="steps-action" style={{position: "relative"}}>
                         {
                             current < steps.length - 1
-                            && <Button type="primary" onClick={() => this.next()}>下一步，填写商品信息</Button>
+                            && <Button
+                                type="primary"
+                                style={{
+                                    position:"absolute",
+                                    right: -15,
+                                    top: -32
+                                }}
+                                onClick={(e) => {
+                                    this.handleSubmit(e)
+                                }}>下一步，填写商品信息</Button>
                         }
                         {
                             current === steps.length - 1
@@ -82,3 +110,7 @@ export default class Temp extends Component {
         )
     }
 }
+
+const createForm = Form.create;
+Temp = createForm()(Temp);//将Form表单,通过props获取到各种方法
+export default Temp
--- React项目/react-mall/src/components/Content/Food/Add/Step/Step.js
+++ React项目/react-mall/src/components/Content/Food/Add/Step/Step.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
-import { Button, Form, Input } from 'antd';
+import { Button, Form, Input, Select, InputNumber } from 'antd';
 import './Add.less';
 
-const createForm = Form.create;
+
 const FormItem = Form.Item;
-function noop() {
-  return false;
-}
+const Option = Select.Option;
+const ButtonGroup = Button.Group;
+
+ 
 /* 
     antd 表单使用
     resetFields() 重置表单内数据
@@ -19,30 +20,19 @@ function noop() {
      如果校验不通过的菜单域不在可见范围内，则自动滚动进可见范围（建议用在Submit）
 
      isFieldValidating	判断一个输入控件是否在校验状态（可以扔到help里面）
+     两种报错方法 validateFields和rule中的min判断条件
+     min默认为 name is require 使用message进行修改
+     
 */
 class StepOne extends Component {
-  constructor() {
+  constructor() {//传入的props需要在constructor接收一下
     super()
   }
   handleReset(e) {
     //resetFields重置表单内容
     e.preventDefault();
-    this.props.form.resetFields();
+    this.props.props.form.resetFields();
   };
-  handleSubmit(e) {
-    //表单内容提交组件,校验并获取一组输入域的值与 Error	
-    e.preventDefault();
-    console.log( this.props.form.getFieldError("name"))
-
-    this.props.form.validateFields((errors, values) => {
-      if (!!errors) {
-        console.log('Errors in form!!!');
-        return;
-      }
-      console.log('Submit!!!');
-    });
-  };
-
   userExists(rule, value, callback) {
     //当做validator函数来用,用来作为判断条件
     if (!value) {
@@ -58,95 +48,124 @@ class StepOne extends Component {
     }
   };
 
-  checkPass(rule, value, callback) {
-    const { validateFields } = this.props.form;
-    if (value) {
-      validateFields(['rePasswd'], { force: true });
-    }
-    callback();
-  };
-
-
-  componentWillReceiveProps(){
-    console.log(this.props.form.getFieldError())
-  };
+  componentDidMount(){
+     // this.props.onRef(this)
+  }
+  // componentWillReceiveProps() {
+  // console.log(this.props.form.getFieldError("name"))
+  //返回值     ["输入1时显示报错"]
+  //          ["菜品名至少为 1个字符"]  message 
+  // };
   render() {
-    const { getFieldProps, getFieldError, isFieldValidating } = this.props.form;
+      const { getFieldProps, getFieldError, isFieldValidating } = this.props.props.form; 
+    //--------------------------------需要-----------------------
     const nameProps = getFieldProps('name', { //第一个参数为ID,第二个为options
       /* 
       getFieldProps options 
       */
       rules: [
-        { required: true, min: 1, message: '菜品名至少为 1 个字符', trigger: 'onblur' },
+        { required: true, min: 1, message: '菜品名至少为 1 个字符' },
         { validator: this.userExists.bind(this) },
+        ,
+        //  {
+        //    -------添加另外限制条件------------
+        //   validator(rule, value, callback, source, options) {
+        //     var errors = [];
+        //     console.log(value, "Xx") 
+        //     if (value == 1) {
+        //       callback("输入1时显示报错");
+        //     } else {
+        //       callback();
+        //       //  成功后显示在页面上的值
+        //       //  ["菜品名至少为 1 个字符", 22222]
+        //     }
+        //   }
+        // }
       ]
     });
-    const textareaProps = getFieldProps('textarea', {
+
+    
+   const selectProps = getFieldProps('select', {
       rules: [
-        { required: true,min: 1, message: '输入内容不能为空' },
+        { required: true, message: '请选择菜品类型' },
       ],
-    });
-    const formItemLayout = {
-      labelCol: { span: 7 },
-      wrapperCol: { span: 12 },
-    };
+    }); 
    
 
+
+    const textareaProps = getFieldProps('textarea', {//textarea 为ID
+      rules: [//判断条件
+        {  message: '输入内容不能为空' },
+      ],
+    });
+    
+   const formItemLayout = {//样式
+      labelCol: { span: 7 },
+      wrapperCol: { span: 12 },
+    }; 
+   
+ 
     return (
-      <Form >
+       <Form >
         <FormItem
           {...formItemLayout}
           label="菜品名称："
           hasFeedback
           help={isFieldValidating('name') ? '校验中...' : (getFieldError('name') || []).join(', ')}
         >
-          <Input {...nameProps} placeholder=" 输入 宫爆鸡丁 看看" 
-/>
+          <Input {...nameProps} placeholder=" 输入 宫爆鸡丁 看看"
+          />
+        </FormItem>
+
+        <FormItem
+          label="菜品类型："
+          {...formItemLayout}
+          required
+        >
+          <Select style={{ width: 200 }}
+            {...selectProps}
+          >
+            <Option value="特色汤面">特色汤面</Option>
+            <Option value="酒水饮料">酒水饮料</Option>
+            <Option value="烧烤系列">烧烤系列</Option>
+            <Option value="美味盖饭">美味盖饭</Option>
+            <Option value="小吃，肉夹馍">小吃，肉夹馍</Option>
+            <Option value="海鲜" disabled>海鲜</Option>
+          </Select>
         </FormItem>
 
         <FormItem
           {...formItemLayout}
-           label="菜品类型："
+          label="菜品价格："
         >
-          <Input {...textareaProps} type="textarea" placeholder="菜品名称" id="textarea" name="textarea" />
+          <InputNumber
+            style={{ width: 320 }}
+            type="textarea"
+            // placeholder="菜品名称"
+            id="textarea"
+            name="textarea"
+          />
         </FormItem>
-        <Button onClick={this.handleReset.bind(this)} > Reset</Button >
-        <Button onClick={this.handleSubmit.bind(this)}> Submit</Button >
+
+        <FormItem
+          id="control-textarea"
+          label="菜品介绍："
+          {...formItemLayout}
+        >
+            <Input id="control-textarea" style={{
+            borderRadius: 6, textAlign: "left", verticalAlign: "top", border: "1px solid #d9d9d9", fontSize: 14, lineHeight: "20px", textIndent: "1em"
+          }} placeholder="Please enter..." cols="45" rows="4"   />
+         </FormItem>
+
+        <ButtonGroup style={{ borderRadius: 15,marginTop: 30}}>
+          <Button 
+          onClick={this.handleReset.bind(this)} 
+          style={{ marginLeft: 400 }}>Reset</Button>
+        </ButtonGroup>
       </Form>
 
     );
-
-
   }
 }
-StepOne = createForm()(StepOne);//将Form表单,通过props获取到各种方法
-
 
 export default StepOne
-
-
-
-
-
-
-
-
-
-// class StepTwo extends Component {
-//   render() {
-//     return (
-//       <div className="stepTwo">
-//         11111111111111111
-//        </div>
-//     )
-//   }
-// }
-// class StepThree extends Component {
-//   render() {
-//     return (
-//       <div className="stepThree">
-//         22222222222222
-//       </div>
-//     )
-//   }
-// }
