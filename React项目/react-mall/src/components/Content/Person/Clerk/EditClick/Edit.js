692a301bb2e121dba4405e0153a719e4dc709fcf
lizijie <562949697@qq.com>
2019-03-07 00:07:19 +0800

    更新

--- React项目/react-mall/src/components/Content/Person/Clerk/EditClick/Edit.js
+++ React项目/react-mall/src/components/Content/Person/Clerk/EditClick/Edit.js
@@ -11,25 +11,20 @@ const ButtonGroup = Button.Group;
 const title = ["姓名", "身份证号", "出生年月", "年龄", "入职时间", "职位"];
 
 class Temp extends Component {
-  handleReset(e) {
-    //resetFields重置表单内容
-    e.preventDefault();
-    this.props.props.form.resetFields();
-  };
-  userExists(rule, value, callback) {
-    //当做validator函数来用,用来作为判断条件
-    if (!value) {
-      callback();
-    } else {
-      setTimeout(() => {
-        if (value === '宫爆鸡丁') {
-          callback([new Error('抱歉，该菜名已被占用。')]);
-        } else {
-          callback();
-        }
-      }, 800);
-    }
-  };
+  // userExists(rule, value, callback) {
+  //   //当做validator函数来用,用来作为判断条件
+  //   if (!value) {
+  //     callback();
+  //   } else {
+  //     setTimeout(() => {
+  //       if (value === '宫爆鸡丁') {
+  //         callback([new Error('抱歉，该菜名已被占用。')]);
+  //       } else {
+  //         callback();
+  //       }
+  //     }, 800);
+  //   }
+  // };
   handleSubmit = (e) => {
     e.preventDefault();
     this.props.form.validateFields((err, values) => {
@@ -38,22 +33,21 @@ class Temp extends Component {
       }
     });
   }
-  normFile = (e) => {
-    console.log('Upload event:', e);
-    if (Array.isArray(e)) {
-      return e;
-    }
-    return e && e.fileList;
+  handlePageClick(){
+    console.log(this.props.history.go(-1))
   }
+
+
   render() {
+    //路由携带过来的数据
+    // console.log(this.props.location.state[0])
+
+    //获取对象中的所有keys数值;
     let arr = Object.keys(this.props.location.state[0])
+
+    //对所有keys数值进行过滤;
     let TempArr = arr.filter((currentValue, index) => currentValue != "num" && currentValue != "sex" && currentValue != "state")
-
-    console.log(this.props.location.state[0])//数据
-    
     const { getFieldProps, getFieldError, isFieldValidating, getFieldDecorator } = this.props.form;
-
-
     const formItemLayout = {//样式
       // span是整体左移 pull是label左移   
       labelCol: { span: 6, pull: 1 },//左侧 offset:3
@@ -109,21 +103,32 @@ class Temp extends Component {
                     initialValue: this.props.location.state[0][TempArr[index]],
                     rules: [
                       { required: true, min: 1, message: '输入内容至少为 1 个字符' },
-                      { validator: this.userExists.bind(this) }
+                      { validator: this.userExists }
                     ]
                   })(
                     <Input />
                   )}
               </FormItem>
             )}
-            <FormItem>
+            <FormItem
+              style={{
+                display: "inline-block", marginRight: 10, marginTop: 10,marginLeft: 160
+                , fontSize: 13, borderRadius: 5,textIndent:"100px"
+              }}>
               <Button
                 ghost="true"
-                style={{ marginRight: 10, fontSize: 13, borderRadius: 5 }}
-                type="primary">
-                21321</Button>
+                type="primary"
+                onClick={this.handlePageClick.bind(this)}	
+                >
+                返回</Button>
             </FormItem>
-            <FormItem> <Button type="primary">21321</Button> </FormItem>
+            <FormItem
+              style={{ display: "inline-block", marginTop: 10 }}
+            >
+              <Button
+                style={{ display: "inline-block" }}
+                type="primary">提交</Button>
+            </FormItem>
           </Form >
         </div>
       </section>
